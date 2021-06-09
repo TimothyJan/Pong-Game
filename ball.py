0014f7503fbbc29052dc24e094b2d8ff3f6a54fc
TimothyJan <jan.timothy@gmail.com>
2021-06-09 16:38:35 -0700

    Update ball and main

--- ball.py
+++ ball.py
@@ -19,19 +19,12 @@ class Ball(Turtle):
     def bounce_y(self):
         self.y_move *= -1
     
-    def bounce_x_right(self):
-        self.x_move *= -(abs(self.x_move))
-        self.move_speed *= 0.9
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9    
     
-    def bounce_x_left(self):
-        self.x_move *= abs(self.x_move)
-        self.move_speed *= 0.9       
-    
-    def reset_position(self, direction):
+    def reset_position(self):
         self.goto(0,0)
         self.move_speed = 0.1 # reset ball move_speed
         # change direction when one side wins
-        if direction == "right":
-            self.bounce_x_left()
-        elif direction == "left":
-            self.bounce_x_right()
+        self.bounce_x()
--- main.py
+++ main.py
@@ -32,22 +32,18 @@ while game_is_on:
     if ball.ycor()>280 or ball.ycor()<-280:
         ball.bounce_y()
     
-    """Detect collision with right paddle"""
-    if ball.distance(r_paddle)<50 and ball.xcor()>320:
-        ball.bounce_x_right()
-    
-    """Detect collision with left paddle"""
-    if ball.distance(l_paddle)<50 and ball.xcor()<-320:
-        ball.bounce_x_left()
+    """Detect collision with right/left paddle"""
+    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()<-320:
+        ball.bounce_x()
     
     """Detect when right paddle misses"""
     if ball.xcor()>380:
-        ball.reset_position(direction = "left")
+        ball.reset_position()
         scoreboard.l_point()
     
     """Detect when left paddle misses"""
     if ball.xcor()<-380:
-        ball.reset_position(direction = "right")   
+        ball.reset_position()   
         scoreboard.r_point()
 
 
